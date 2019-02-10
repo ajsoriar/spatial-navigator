f3ab3e4f98557aa3d76f16928564a47b10028642
asr <ajsoriar@gmail.com>
2019-02-10 19:54:15 +0100

    Improve demos

--- demo/demo-01/script.js
+++ demo/demo-01/script.js
@@ -40,47 +40,6 @@ App.doStuff = function(){
 
 $( document ).ready(function() {
 
-    console.log( "ready!" );
-    nav.reset();
-    appWindow.resize();
-    App.doStuff();
-
-    // atach events to demo buttons
-    $( "#btn-up" ).mouseenter(function() {
-        nav.calculateDecissionPreviewAndShow( 1);
-    });
-    $( "#btn-down" ).mouseenter(function() {
-        nav.calculateDecissionPreviewAndShow( 3);
-    });
-    $( "#btn-left" ).mouseenter(function() {
-        nav.calculateDecissionPreviewAndShow( 2);
-    });
-    $( "#btn-right" ).mouseenter(function() {
-        nav.calculateDecissionPreviewAndShow( 4);
-    });
-
-    $( "#btn-up" ).mouseleave(function() {
-        nav.clearPreview( 1);
-    });
-    $( "#btn-down" ).mouseleave(function() {
-        nav.clearPreview( 3);
-    });
-    $( "#btn-left" ).mouseleave(function() {
-        nav.clearPreview( 2);
-    });
-    $( "#btn-right" ).mouseleave(function() {
-        nav.clearPreview( 4);
-    });
-
-    // more
-    
-    $( "#btn-random" ).mouseleave(function() {
-        //nav.clearPreview();
-        nav.applyRandomDecision();
-    });
+    console.log('jQuery document ready! 1');
 
 });
-
-
-
-
