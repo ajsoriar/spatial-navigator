0924bb420935319fd81cce7a4f321f917a76cf38
asr <ajsoriar@gmail.com>
2019-12-01 14:18:38 +0100

    Zone filter

--- src/spatial-navigator.js
+++ src/spatial-navigator.js
@@ -112,46 +112,124 @@
     };
 
     var Filters = {
-        nearestTo: function(x,y,targets){
+        filter_targets_in_area_by_max_min: function(x1, y1, x2, y2, targets) {
+            console.log("fom: ", x1 , ", ", y1 );
+            console.log("to: ", x2 , ", ", y2 );
+            var arr = [],
+                lon = targets.length;
 
+            for (var i = 0; i < lon; i++){
+                if ( targets[i].x1 >= x1 && targets[i].x2 <= x2  ){
+                    if ( targets[i].y1 >= y1 && targets[i].y2 <= y2  ){
+                        arr.push( targets[i]);
+                    }
+                }
+            }
+
+            return arr;
         },
-        filter_nearest_to: function(x,y, targets) {
+        filter_targets_in_area_by_center_xy: function(x1, y1, x2, y2, targets) {
+            var arr = [];
+            for (var i = 0; i < targets.length; i++){
+                if ( targets[i].cx >= x1 && targets[i].cx <= x2  ){
+                    if ( targets[i].cy >= y1 && targets[i].cy <= y2  ){
+                        arr.push( targets[i]);
+                    }
+                }
+            }
+            return arr;
+        },
+        filter_targets_nearest_to_xy: function(x,y, targets) {
 
+            var current = null,
+                i = 0,
+                lon = targets.length,
+                ld = null,
+                d = null;
+
+            for (i; i < lon; i++) {
+                if (i === 0) {
+
+                    current = targets[0];
+                    ld = Utils.getDistance ( targets[0].cx, targets[0].cy, x, y );
+
+                } else {
+
+                    d = Utils.getDistance ( targets[i].cx, targets[i].cy, x, y );
+                    if ( d < ld ) {
+
+                        current = targets[i];
+                        ld = d;
+                    }
+                }
+            }
+
+            return current;
         }
     };
 
-    var focusArea = function(areaId, v, h ){
+    var focusArea = function(areaId, v, h ){ // focusArea('id', 'top', 'left');
 
         if ( areaId === undefined || areaId === null ) {
             console.error("div layer id needed!");
             return -1
         }
 
-        if ( v === null ) v = 'top';
+        if ( !v ) v = 'top';
 
-        if ( h === null ) h = 'left';
+        if ( !h ) h = 'left';
 
         var el = document.getElementById( areaId );
         if ( el === null ) return -1
 
+        var domRect = el.getBoundingClientRect();
+        var p = {};
+        p.x1 = domRect.left;
+        p.y1 = domRect.top;
+        p.x2 = domRect.right;
+        p.y2 = domRect.bottom;
+
         // get position of top-left corner of el
-        var area = {
-            corner: {
-                x: 0,
-                y: 0
+        var corner = {};
+
+        if ( v === 'top' ){
+            if ( h === 'left'){
+                console.log("top left");
+                corner.x = p.x1;
+                corner.y = p.y1;
+            } else { // right
+                console.log("top right");
+                corner.x = p.x2;
+                corner.y = p.y1;
             }
-        };
+        } else { // bottom
+            if ( h === 'left'){
+                console.log("bottom left");
+                corner.x = p.x1;
+                corner.y = p.y2;
+            } else { // right
+                console.log("bottom right");
+                corner.x = p.x2;
+                corner.y = p.y2;
+            }
+        }
 
         // get all elemient centers in the area of el
         refreshFocusableMap();
         var targets = calculateAllDistances();
-        targets = Filters.filter_targets_in_area(a,b,targets);
+        targets = Filters.filter_targets_in_area_by_max_min(p.x1, p.y1, p.x2, p.y2, targets);
 
         // get the nearest center to top-left corner
-        Filters.filter_nearest_to(area.corner.x, area.corner.y, tatgets);
+        var targetElement = Filters.filter_targets_nearest_to_xy(corner.x, corner.y, targets);
+
+        console.log( targetElement );
+
+        nav.focusById( targetElement.id );
+
+        return targetElement;
     };
 
-    //topLeft: focusInArea, //(areaId,'top','le //ft')
+    //topLeft: focusArea, //(areaId,'top','le //ft')
 
     var focusItem = function(num) {
 
@@ -212,13 +290,15 @@
         var current = nav.current
 
         nav.focusableTargets = calculateDistances(elmts, current);
+
+        return nav.focusableTargets;
     }
 
     var calculateDistances = function(arrOfElements, currentEl) {
 
-        console.log(" --- calculateDistances()");
+        console.log(" calculateDistances()");
         console.log(" currentEl: ", currentEl);
-        console.log(" arrOfElements: ", arrOfElements);
+        //console.log(" arrOfElements: ", arrOfElements);
 
         var resultsArr = [];
         for (var i = 0; i < arrOfElements.length; i++) {
@@ -235,14 +315,17 @@
             p.cx = domRect.left + p.w / 2;
             p.cy = domRect.top + p.h / 2;
             p.distance = Utils.getDistance(currentEl.cx, currentEl.cy, p.cx, p.cy);
-            if (p.distance === 0) continue;
+            if (p.distance === 0) continue; // exclude the current selected element
             p.angle = Utils.getAngle();
             p.distance_axis_x = Utils.substract(p.cx, currentEl.cx);
             p.distance_axis_y = Utils.substract(p.cy, currentEl.cy);
             p.slope = null;
             p.group = null;
+            p.area = null;
             resultsArr.push(p);
         }
+
+        console.log(" resultsArr: ", resultsArr);
 
         return resultsArr;
     };
@@ -666,11 +749,12 @@
             Utils.addClass(nav.current.el, "focused");
         },
         focusById: focusById,
-        focus: {
-            byId: focusById,
-            topRight: focusArea,
-            topLeft: focusArea
-        },
+        // focus: {
+        //     byId: focusById,
+        //     topRight: focusArea,
+        //     topLeft: focusArea
+        // },
+        focusArea: focusArea, // nav.focusArea('zone-1')
         map:{
             draw: function(){
 
